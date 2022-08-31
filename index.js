da0a8a3eaa2657e25965e0dc13d1bcd33efb69d0
EvgeniaRizoudi <rizoudievgenia@hotmail.com>
2022-08-31 16:12:00 +0300

    Add more comments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* Data from API */
+/* Data from API: Placing the correct API key makes the widget functional*/
 const apiKey = prompt("Please paste the API key");
 const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=40.58725980318928&lon=22.948223362612612&exclude=hourly,minutely&appid=${apiKey}&units=metric`
 
@@ -8,7 +8,7 @@ async function fetchData(url){
     return data;
 }
 
-/* Current Weather Card */
+/* Current Weather Card: Pushes the desirable data to the corresponding HTML elements*/
 function getTodaysWeather(todays_data){    
         $ ("#current-temp").text(Math.round(todays_data.temp) + " ");
         $(".temp-feeling").text(`Feeling ${Math.round(todays_data.feels_like)}℃`);
@@ -19,20 +19,23 @@ function getTodaysWeather(todays_data){
         $(".wind-deg").text(`Wind Direction ${todays_data.wind_deg}deg`);
 }
 
-/* Changing Gif */   
+/* Changing Gif: Alters the background Gif according to the current weather*/   
 function changeBgImg(todays_data_weather){
     $("#temperature-card").css("background-image", `url(img/${todays_data_weather[0].icon}.gif)`);
 }
 
-/* Forecast */
+/* Forecast*/
+/* API dt to string date*/  
 function dateToString(date){
    return new Date(date.dt*1000).toDateString();
 }
 
+/* Pushes the date to the buttons*/
 function dateToButton(next_dates,i){
     $((".dateBtn" + i)).text(dateToString(next_dates[i]));
 } 
-    
+   
+/*Pushes the desirable data to the corresponding HTML elements*/
 function getNextDatesData(date){
     const meanValueTemp = Math.round(date.temp.day + date.temp.night) / 2;
     const meanValueFeel = Math.round(date.feels_like.day + date.feels_like.night) / 2;
@@ -45,8 +48,7 @@ function getNextDatesData(date){
     $(".wind-deg-forecast").text(`Wind Direction ${date.wind_deg}deg`);
 }
 
-/* API Images */
-
+/* API Images: Fetches the correct weather image from the API*/
 function getTodaysImgFromApi(data){
     const imgToday = data.current.weather[0].icon;
     const imgUrlToday = `http://openweathermap.org/img/wn/${imgToday}@2x.png`;
@@ -60,7 +62,7 @@ function getNextDaysImgFromApi(data,i){
 }
 
 
-/* Max Temp Chart */
+/* Max Temp Chart: Presents the maximum temperature for the next seven days using the API data*/
 function maxTempChart(dates){
   const ctx = $("#maxTempChart");
   const myChart = new Chart(ctx, {
@@ -101,21 +103,15 @@ function maxTempChart(dates){
         color: 'rgba(251, 251, 251)',
     }
 });}
-/* Go Back Btn */
-  function goBackButton(){
-    $(".go-back-btn").click(function(){
+
+/* Calling All: Invoking all the functions needed to run the widget*/
+$(".forecast-info").hide();
+
+$(".go-back-btn").click(function(){
         $(".collapse-item").collapse('toggle');
         $(".forecast-info").hide();
         $("#temperature-card").show();  
-    })
-}                              
-
- 
-
-/* Calling All */
-$(".forecast-info").hide();
-
-goBackButton();
+})
 
 $(".details").click(function(){
     $("#weather-det").show();
@@ -132,21 +128,19 @@ $(".chart-btn").click(function(){
     $("#maxTempChart").show();
 }) 
 
-data = fetchData(apiUrl).then(data => {
-      getTodaysWeather(data.current);
-      $(".date").text(dateToString(data.current,0));
-      for(let i=0; i<8; i++){
+fetchData(apiUrl).then(data => {
+    getTodaysWeather(data.current);
+    $(".date").text(dateToString(data.current,0));
+    for(let i=0; i<8; i++){
         dateToButton(data.daily,i);
-        btnClick(i);
-        }
+        $((".dateBtn" + i)).click(function(){
+        getNextDatesData(data.daily[i]);
+        getNextDaysImgFromApi(data,i);
+        }); 
+    }
     getTodaysImgFromApi(data);
     maxTempChart(data.daily); 
-    changeBgImg(data.current.weather);
-function btnClick(i){ 
-        $((".dateBtn" + i)).click(function(){
-            getNextDatesData(data.daily[i]);
-            getNextDaysImgFromApi(data,i);
-        })};
+    changeBgImg(data.current.weather);      
 });    
         
 
